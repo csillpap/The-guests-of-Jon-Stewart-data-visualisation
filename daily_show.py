6165b9635445e9f6dfde44a8c7131ece230cc579
Csilla Pap <csill.pap@gmail.com>
2017-09-08 13:49:21 +0100

    Set up the database connection and collection call

--- daily_show.py
+++ daily_show.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'DailyShow'
+COLLECTION_NAME = 'guests_data'
 
 
 @app.route('/')
@@ -8,5 +15,31 @@ def hello_world():
     return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/DailyShow/guests_data")
+def dailyshow_guests():
+    """
+    A Flask view to serve the project data from
+    MongoDB in JSON format.
+    """
+
+    # A constant that defines the record fields that we wish to retrieve.
+    FIELDS = {
+        '_id': False, 'YEAR': True, 'GoogleKnowlege_Occupation': True,
+        'Show': True, 'Group': True,
+        'Raw_Guest_List': True,
+    }
+
+    # Open a connection to MongoDB using a with statement such that the
+    # connection will be closed as soon as we exit the with statement
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # and limit the the results to 55000
+        projects = collection.find(projection=FIELDS, limit=55000)
+        # Convert projects to a list in a JSON object and return the JSON data
+        return json.dumps(list(projects))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
