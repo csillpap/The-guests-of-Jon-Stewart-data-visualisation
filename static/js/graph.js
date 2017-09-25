ea97ef63b720427a64d0357ae9532d158dd50331
Csilla Pap <csill.pap@gmail.com>
2017-09-25 14:35:10 +0100

    Create series chart for the top occupation groups
    Create a series chart to show how the 3 biggest occupation groups - Acting, Media, Politician - were changing over time

--- static/js/graph.js
+++ static/js/graph.js
@@ -39,6 +39,9 @@ function makeGraphs(error, projectsJson) {
    var showDim2 = ndx.dimension(function (d) {
        return d["Show"];
    });
+   var occupationGroupWithYearDim = ndx.dimension(function(d) {
+			return [d["YEAR"],d["Group"]];
+	});
 
 
    //Calculate metrics
@@ -47,11 +50,31 @@ function makeGraphs(error, projectsJson) {
    var specificOccupations = specificOccupationDim.group();
    var guestGroup = guestDim.group();
    var showGroup = showDim.group();
+   var occupationGroupByYear = occupationGroupWithYearDim.group();
 
+   var resultGroupByYear = occupationGroupByYear.all();
+
+   var GroupYearValue = [];
+   var j = 0;
+   for (var i in resultGroupByYear) {
+		if (resultGroupByYear[i].key[1]==="Acting" || resultGroupByYear[i].key[1]==="Media" || resultGroupByYear[i].key[1]==="Politician") {
+			GroupYearValue[j] = {year: resultGroupByYear[i].key[0], group: resultGroupByYear[i].key[1], value: resultGroupByYear[i].value };
+			j++;
+		}
+	}
+
+    var	addT = function(p, d){ return p + d.value;};
+	var remT = function(p, d){ return p - d.value;};
+	var ini = function(){ return 0;};
+	var cf = crossfilter(GroupYearValue);
+	var time_field = cf.dimension(function(d) { return [d.year, d.group];});
+	var time_fields = time_field.group().reduce( 	addT, remT, ini );
+	var extent = d3.extent(GroupYearValue, function(d){return d.year;});
 
    // Charts
    var occupationGroupChart = dc.pieChart("#occupation-group-chart");
    var specificOccupationChart = dc.rowChart("#specific-occupation-chart");
+   var topOccupationGroupsChart = dc.seriesChart("#top-occupation-groups-chart");
    guest_datatable = dc.dataTable('#guest-datatable');
    show_datatable = dc.dataTable("#show-datatable");
 
@@ -87,6 +110,23 @@ function makeGraphs(error, projectsJson) {
        .dimension(specificOccupationDim)
        .group(specificOccupations)
        .xAxis().ticks(4);
+
+   topOccupationGroupsChart
+		.width(1000)
+		.height(400)
+		.x(d3.scale.linear().domain(extent))
+		.brushOn(false)
+		.xAxisLabel("Years")
+		.yAxisLabel("Count")
+		.clipPadding(10)
+		.elasticY(true)
+		.dimension(time_field)
+		.group(time_fields)
+		.seriesAccessor(function(d) { return  d.key[1]; })
+		.keyAccessor(function(d) {return d.key[0];})
+		.renderHorizontalGridLines(true)
+		.renderVerticalGridLines(true)
+		.legend(dc.legend().x(0.35*600).y(260).itemHeight(13).gap(15));
 
    guest_datatable
        .dimension(guestDim2)
